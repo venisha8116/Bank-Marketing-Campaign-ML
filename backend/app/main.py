db68ec38083111e627ff48f54aad0aeaf4d1a411
Venisha Ambasana <venishaambasana@gmail.com>
2026-09-25 14:07:12 +0530

    Allow frontend CORS requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,10 +24,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"],
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
